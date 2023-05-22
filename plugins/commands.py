5d542ff99247facf32676b9462617dd3d2ce1b17
BLVCK ANGEL <83200567+BLVCK-ANGEL@users.noreply.github.com>
2023-05-22 10:56:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -528,7 +528,7 @@ async def settings(client, message):
                     callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
                 ),
                 InlineKeyboardButton(
-                    'Aᴜᴛᴏ Sᴇɴᴅ' if settings["botpm"] else 'Mᴀɴᴜᴀʟ Sᴛᴀʀᴛ',
+                    'Mᴀɴᴜᴀʟ Sᴛᴀʀᴛ' if settings["botpm"] else 'Aᴜᴛᴏ Sᴇɴᴅ',
                     callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
                 ),
             ],
