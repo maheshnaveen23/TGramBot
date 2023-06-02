cd8e687065853a1fe89188c9294f2ba6d4c49c73
Susil2510 <132734591+Susil2510@users.noreply.github.com>
2023-06-02 20:00:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -72,7 +72,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>Yᴏᴜ Nᴇᴇᴅ Tᴏ Rᴇǫᴜᴇsᴛ Tʜɪs Gʀᴏᴜᴘ @SRJ_TELEFLIX !</b>")
+    await message.reply_text("<b>Yᴏᴜ ɴᴇᴇᴅ ᴛᴏ ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ ғᴏʀ ᴍᴏᴠɪᴇs @RSK_MOVIEz ﹗﹗</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
