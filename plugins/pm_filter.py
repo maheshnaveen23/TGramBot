a229e451f264fd72be8b2827b7d11260541b29c6
Susil2510 <132734591+Susil2510@users.noreply.github.com>
2023-06-05 15:50:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1346,7 +1346,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.MOVIES_TXT.format(temp.B_NAME),
-            reply_markup=reply_markup
+            reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
     elif query.data == "source":
