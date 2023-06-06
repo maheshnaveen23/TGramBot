0c190976e9ac547aed9df1ab91b6564939b1404a
Susil2510 <132734591+Susil2510@users.noreply.github.com>
2023-06-06 23:57:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1257,7 +1257,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.ABOUT_TXT.format(temp.B_NAME),
+            text=script.ABOUT_TXT.format,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
